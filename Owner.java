1ff98806a72b9efa77c9ab3b58282432d947bef6
Henry Baillaregon <henrygbaillargeon@gmail.com>
2025-03-17 09:04:01 -0400

    Update Owner.java
    Enhancements Implemented

--- Owner.java
+++ Owner.java
@@ -1,18 +1,26 @@
+package food;
+
+
 public class Owner {
+	
+	// Private fields to hold owner's name, contact number, and activity level
     private String name;
     private String contactNumber;
     private String activityLevel;
 
+    // Constructor 
     public Owner(String name, String contactNumber, String activityLevel) {
         this.name = name;
         this.contactNumber = contactNumber;
         this.activityLevel = activityLevel;
     }
 
+    // Getter for owners name
     public String getName() {
         return name;
     }
 
+    // toString for owner's details to string for output
     @Override
     public String toString() {
         return String.format("Owner's Name: %s, Contact: %s, Activity Level: %s",
